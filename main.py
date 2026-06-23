6bf4cd8f1965658308b06c02ac71f362d100c93c
Khoa Zr <duongcongkhoa171006@gmail.com>
2026-06-23 09:36:24 +0700

    feat: Tái cấu trúc quy trình thu thập và xử lý dữ liệu để hỗ trợ nhiều mã chứng khoán khác nhau.

--- main.py
+++ main.py
@@ -7,16 +7,14 @@ from src.process.stock_processor import(
 )
 import pandas as pd
 def main():
-    symbol = "TCB.VN"
-    df = get_data_today(symbol)
-    data_file_path = f"data/raw/{symbol}.csv"
-    df.to_csv(
-        data_file_path
-    )
-    df_process = pd.read_csv(data_file_path)
-    df_process = process_stock_data(df_process,symbol)
-    data_processed_file_path = f"data/raw/{symbol}_processed.csv"
-    df_process.to_csv(data_processed_file_path, index=False)
-    print(df_process.head())
+    all_data = []
+    symbols = ["TCB.VN","FPT.VN"]
+    for symbol in symbols:
+        df = get_data_today(symbol)
+        df_process = process_stock_data(df.copy(),symbol)
+        path_df_process = f"data/raw/data_a_day.csv"
+        all_data.append(df_process)
+    final_data = pd.concat(all_data, ignore_index=True)
+    final_data.to_csv(path_df_process,index = False)
 if __name__ == "__main__":
     main()
