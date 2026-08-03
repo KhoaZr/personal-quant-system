c7d7dda389788082fd1ae442e5122cac379a067f
Khoa Zr <duongcongkhoa171006@gmail.com>
2026-08-03 18:49:11 +0700

    feat(database): thêm thiết kế lược đồ cơ sở dữ liệu ban đầu

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from src.fetch.yahoo_finance import(
-    get_data_today
+    get_data_today,
+    get_data
 )
 
 from src.process.stock_processor import(
@@ -8,13 +9,16 @@ from src.process.stock_processor import(
 import pandas as pd
 def main():
     all_data = []
-    symbols = ["TCB.VN","FPT.VN","BVH.VN","BVH.VN","GAS.VN","HPG.VN","MSN.VN","MWG.VN","PLX.VN","POW.VN","SAB.VN","VCB.VN","VIC.VN","VJC.VN","VNM.VN"]
-    for symbol in symbols:
-        df = get_data_today(symbol)
-        df_process = process_stock_data(df.copy(),symbol)
-        path_df_process = f"data/raw/data_a_day.csv"
-        all_data.append(df_process)
-    final_data = pd.concat(all_data, ignore_index=True)
-    final_data.to_csv(path_df_process,index = False)
+    # symbols = ["TCB.VN","FPT.VN","BVH.VN","BVH.VN","GAS.VN","HPG.VN","MSN.VN","PLX.VN","POW.VN","SAB.VN","VCB.VN","VIC.VN"]
+    # for symbol in symbols:
+    #     df = get_data_today(symbol)
+    #     df_process = process_stock_data(df.copy(),symbol)
+    #     path_df_process = f"data/raw/data_a_day.csv"
+    #     all_data.append(df_process)
+    # final_data = pd.concat(all_data, ignore_index=True)
+    # final_data.to_csv(path_df_process,index = False)
+    symbol = "FPT.VN"
+    df = get_data(symbol)
+    df.to_csv(f"data/raw/{symbol}.csv")
 if __name__ == "__main__":
     main()
--- src/fetch/yahoo_finance.py
+++ src/fetch/yahoo_finance.py
@@ -1,4 +1,5 @@
 import yfinance as yf
+from datetime import datetime
 import pandas as pd
 def get_data_today(symbol):
     df = yf.download(
@@ -10,3 +11,15 @@ def get_data_today(symbol):
         df.columns = df.columns.get_level_values(0)
     return df
 
+def get_data(symbol):
+    df = yf.download(
+        tickers=symbol,
+        start="2010-01-01",
+        end=datetime.today().strftime("%Y-%m-%d"),
+        interval="1d",
+        auto_adjust=False,
+        progress=False
+    )
+    if isinstance(df.columns, pd.MultiIndex):
+        df.columns = df.columns.get_level_values(0)
+    return df
